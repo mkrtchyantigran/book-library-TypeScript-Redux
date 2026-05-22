952ce527be0df9d5118025642d59e220683b0881
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-05-22 19:13:55 +0200

    React initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+export default function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library App</h1>
+
+      </header>
+    </div>
+  );
+}
+
