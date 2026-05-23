b0b0ee5aa42ac0a2b65d7e407ca1c720abc80f49
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-05-24 01:13:01 +0200

    import BookForm, BookList, Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 
 import "./App.css"
+import BookForm from "./components/bookForm/BookForm.tsx";
+import BookList from "./components/BookList/BookList";
+import Filter from "./components/Filter/Filter";
+
 export default function App() {
 
   return (
     <div className="app">
       <header className="app-header">
         <h1>Book Library App</h1>
-
       </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          <Filter />
+          <BookList />
+        </div>
+      </main>
     </div>
   );
 }
